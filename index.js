df23cfdddc4d0418ade31f670ef7d89432f8e015
william Murphy <william@datatitian.com>
2019-03-09 14:18:42 -0600

    automatically create video/canvas elements and make video passthrough display togglable with addl modebar button

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import 'tracking'
 import 'tracking/build/data/face'
 import FaceTrackingCamera from './src/face-tracking-camera.js'
 export default FaceTrackingCamera
-console.log('Portrait icon copyright Font Awesome licensed under CC Attribution 4.0, https://fontawesome.com/license')
+console.log('Portrait and Super Power icons copyright Font Awesome licensed under CC Attribution 4.0, https://fontawesome.com/license')
--- src/face-tracking-camera.js
+++ src/face-tracking-camera.js
@@ -2,13 +2,47 @@ import MovingAverage from './moving-average.js'
 import { Matrix4 } from 'three/src/math/Matrix4'
 import { Quaternion } from 'three/src/math/Quaternion'
 import { Vector3 } from 'three/src/math/Vector3'
+import StyleSheet from './dyss'
+
+const sheet = new StyleSheet()
+// css classes
+const classVideoContainer = 'cl-face-tracking-video-container'
+const classTrackingActive = 'cl-face-tracking-active'
+const classModebarActive = 'cl-modebar-face-tracking-active'
+const classVideoActive = 'cl-face-tracking-video-active'
+const classTrackingBtnActive = 'cl-face-tracking-button-active'
+const classVideoBtnActive = 'cl-face-tracking-video-button-active'
+
+const passthroughBtnName = 'Magic Portal Camera'
+sheet.add(`.${classTrackingBtnActive} path, .${classVideoBtnActive} path`, {
+  fill: 'rgba(68, 68, 68, 0.7) !important' // override plotly's id based selector
+})
+sheet.add(`a[data-title="${passthroughBtnName}"]`, {
+  display: 'none'
+})
+sheet.add(`.${classModebarActive} a[data-title="${passthroughBtnName}"]`, {
+  display: 'inline'
+})
+sheet.add(`.${classVideoContainer}`, {
+  position: 'fixed',
+  bottom: '0',
+  left: '50%',
+  transform: 'translateX(-50%)',
+  width: '320px',
+  height: '240px',
+  opacity: '0',
+  pointerEvents: 'none'
+})
+sheet.add(`.${classVideoContainer}>*`, {
+  position: 'absolute'
+})
+sheet.add(`.${classVideoContainer}.${classTrackingActive}.${classVideoActive}`, {
+  opacity: '1',
+  pointerEvents: 'auto'
+})
 
 class FaceTrackingCamera {
-  constructor (plotEl, videoEl, cameraSettings, trackerSettings, debugCanvas) {
-    if (!plotEl || !plotEl.layout) {
-      throw new Error('FaceTrackingCamera requires a rendered plotly element')
-    }
-
+  constructor (cameraSettings, trackerSettings) {
     this.cameraSettings = cameraSettings || {}
     this.cameraSettings.range = this.cameraSettings.range || { x: 3, y: 3, z: 3 }
     this.cameraSettings.smoothingDecay = this.cameraSettings.smoothingDecay || 300
@@ -20,13 +54,16 @@ class FaceTrackingCamera {
     this.trackerSettings.trackerStepSize = this.trackerSettings.trackerStepSize || 1
     this.trackerSettings.trackerEdgesDensity = this.trackerSettings.trackerEdgesDensity || 0.1
 
-    this.videoEl = videoEl
-    this.plotEl = plotEl
-
-    if (debugCanvas) {
-      this.canvas = debugCanvas
-      this.context = this.canvas.getContext('2d')
-    }
+    // create a toggleable video element to process/show the webcam image
+    this.videoContainer = document.createElement('div')
+    this.videoContainer.innerHTML = `
+    <video width="320" height="240" preload muted autoplay webkit-playsinline playsinline></video>
+    <canvas width="320" height="240"></canvas>`
+    this.videoContainer.classList.add(classVideoContainer)
+    document.body.appendChild(this.videoContainer)
+    this.videoEl = this.videoContainer.querySelector('video')
+    this.canvas = this.videoContainer.querySelector('canvas')
+    this.context = this.canvas.getContext('2d')
 
     this.faceCenter = { x: 0, y: 0 }
 
@@ -34,56 +71,82 @@ class FaceTrackingCamera {
     this.vec = new Vector3()
     this.target = new Vector3()
     this.quat = new Quaternion()
+  }
+
+  addToPlot (plotEl) {
+    if (!plotEl || !plotEl.layout) {
+      throw new Error('FaceTrackingCamera requires a rendered plotly element')
+    }
+    this.plotEl = plotEl
     window.Plotly.react(plotEl, plotEl.data, plotEl.layout, {
-      modeBarButtonsToAdd: [{
-        name: 'Magic Portal View',
-        icon: {
-          width: 1000,
-          height: 1000,
-          path: 'M336 0H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zM192 128c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zm112 236.8c0 10.6-10 19.2-22.4 19.2H102.4C90 384 80 375.4 80 364.8v-19.2c0-31.8 30.1-57.6 67.2-57.6h5c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h5c37.1 0 67.2 25.8 67.2 57.6v19.2z',
-          transform: 'matrix(1.9 0 0 1.75 0 100)'
+      modeBarButtonsToAdd: [[
+        {
+          name: passthroughBtnName,
+          icon: {
+            width: 1000,
+            height: 1000,
+            path: 'M336 0H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zM192 128c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zm112 236.8c0 10.6-10 19.2-22.4 19.2H102.4C90 384 80 375.4 80 364.8v-19.2c0-31.8 30.1-57.6 67.2-57.6h5c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h5c37.1 0 67.2 25.8 67.2 57.6v19.2z',
+            transform: 'matrix(1.9 0 0 1.75 0 100)'
+          },
+          click: this.handlePassthroughClick.bind(this)
         },
-        click: this.handleClick.bind(this)
-      }]
+        {
+          name: 'Magic Portal View',
+          icon: {
+            width: 1000,
+            height: 1000,
+            path: 'M448 32c-83.3 11-166.8 22-250 33-92 12.5-163.3 86.7-169 180-3.3 55.5 18 109.5 57.8 148.2L0 480c83.3-11 166.5-22 249.8-33 91.8-12.5 163.3-86.8 168.7-179.8 3.5-55.5-18-109.5-57.7-148.2L448 32zm-79.7 232.3c-4.2 79.5-74 139.2-152.8 134.5-79.5-4.7-140.7-71-136.3-151 4.5-79.2 74.3-139.3 153-134.5 79.3 4.7 140.5 71 136.1 151z',
+            transform: 'matrix(1.75 0 0 1.75 0 100)'
+          },
+          click: this.handleActivationClick.bind(this)
+        }
+      ]]
     })
   }
-  handleClick (plot, event) {
+
+  drawDebugRectangles (event) {
+    const context = this.context
+    const canvas = this.canvas
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    event.data.forEach(function (rect) {
+      context.strokeStyle = '#a64ceb'
+      context.strokeRect(rect.x, rect.y, rect.width, rect.height)
+      context.font = '11px Helvetica'
+      context.fillStyle = '#fff'
+    })
+  }
+
+  handleActivationClick (plot, event) {
     const btn = event.currentTarget
     if (this.isTracking) {
       this.stopTracking()
-      btn.classList.remove('cl-face-tracking-active')
-      btn.querySelector('path').style.fill = ''
+      btn.classList.remove(classTrackingBtnActive)
+      btn.parentElement.classList.remove(classModebarActive)
+      this.videoContainer.classList.remove(classTrackingActive)
     } else {
       this.startTracking()
-      btn.classList.add('cl-face-tracking-active')
-      btn.querySelector('path').style.fill = 'rgba(68, 68, 68, 0.7)'
+      btn.classList.add(classTrackingBtnActive)
+      btn.parentElement.classList.add(classModebarActive)
+      this.videoContainer.classList.add(classTrackingActive)
+    }
+  }
+
+  handlePassthroughClick (plot, event) {
+    const btn = event.currentTarget
+    if (btn.classList.contains(classVideoBtnActive)) {
+      btn.classList.remove(classVideoBtnActive)
+      this.videoContainer.classList.remove(classVideoActive)
+    } else {
+      btn.classList.add(classVideoBtnActive)
+      this.videoContainer.classList.add(classVideoActive)
     }
   }
 
   onTracked (event) {
-    // const object3D = this.el.object3D
     const webcam = this.videoEl
     var now = Date.now()
     if (this.canvas) {
-      const context = this.context
-      const canvas = this.canvas
-      context.clearRect(0, 0, canvas.width, canvas.height)
-      event.data.forEach(function (rect) {
-        context.strokeStyle = '#a64ceb'
-        context.strokeRect(rect.x, rect.y, rect.width, rect.height)
-        context.font = '11px Helvetica'
-        context.fillStyle = '#fff'
-        context.fillText(
-          'x: ' + rect.x + 'px',
-          rect.x + rect.width + 5,
-          rect.y + 11
-        )
-        context.fillText(
-          'y: ' + rect.y + 'px',
-          rect.x + rect.width + 5,
-          rect.y + 22
-        )
-      })
+      this.drawDebugRectangles(event)
     }
     const face = event.data[0]
     if (
